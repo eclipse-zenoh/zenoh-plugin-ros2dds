c787831e727bd73f887d179ac74061e54321d725
Julien Enoch <julien.e@zettascale.tech>
2024-01-09 17:17:35 +0100

    Bump to clap 4.4 (#49)

--- zenoh-bridge-ros2dds/src/main.rs
+++ zenoh-bridge-ros2dds/src/main.rs
@@ -12,184 +12,17 @@
 //   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
 //
 use async_liveliness_monitor::LivelinessMonitor;
-use clap::{App, Arg};
+use bridge_args::BridgeArgs;
+use clap::Parser;
 use std::time::{Duration, SystemTime};
-use zenoh::config::{Config, ModeDependentValue};
-use zenoh::prelude::*;
+use zenoh::config::Config;
 
-lazy_static::lazy_static!(
-    pub static ref DEFAULT_DOMAIN_STR: String = zenoh_plugin_ros2dds::config::DEFAULT_DOMAIN.to_string();
-);
+mod bridge_args;
+mod zenoh_args;
 
-macro_rules! insert_json5 {
-    ($config: expr, $args: expr, $key: expr, if $name: expr) => {
-        if $args.occurrences_of($name) > 0 {
-            $config.insert_json5($key, "true").unwrap();
-        }
-    };
-    ($config: expr, $args: expr, $key: expr, if $name: expr, $($t: tt)*) => {
-        if $args.occurrences_of($name) > 0 {
-            $config.insert_json5($key, &serde_json::to_string(&$args.value_of($name).unwrap()$($t)*).unwrap()).unwrap();
-        }
-    };
-    ($config: expr, $args: expr, $key: expr, for $name: expr, $($t: tt)*) => {
-        if let Some(value) = $args.values_of($name) {
-            $config.insert_json5($key, &serde_json::to_string(&value$($t)*).unwrap()).unwrap();
-        }
-    };
-}
-
-fn parse_args() -> (Config, Option<f32>) {
-    let mut app = App::new("zenoh bridge for DDS")
-        .version(zenoh_plugin_ros2dds::GIT_VERSION)
-        .long_version(zenoh_plugin_ros2dds::LONG_VERSION.as_str())
-        //
-        // zenoh related arguments:
-        //
-        .arg(Arg::from_usage(
-r"-i, --id=[HEX_STRING] \
-'The identifier (as an hexadecimal string, with odd number of chars - e.g.: 0A0B23...) that zenohd must use.
-WARNING: this identifier must be unique in the system and must be 16 bytes maximum (32 chars)!
-If not set, a random UUIDv4 will be used.'"
-            ))
-        .arg(Arg::from_usage(
-r"-m, --mode=[MODE]  'The zenoh session mode.'")
-            .possible_values(["peer", "client"])
-            .default_value("peer")
-        )
-        .arg(Arg::from_usage(
-r"-c, --config=[FILE] \
-'The configuration file. Currently, this file must be a valid JSON5 file.'"
-            ))
-        .arg(Arg::from_usage(
-r"-l, --listen=[ENDPOINT]... \
-'A locator on which this router will listen for incoming sessions.
-Repeat this option to open several listeners.'"
-                ),
-            )
-        .arg(Arg::from_usage(
-r"-e, --connect=[ENDPOINT]... \
-'A peer locator this router will try to connect to.
-Repeat this option to connect to several peers.'"
-            ))
-        .arg(Arg::from_usage(
-r"--no-multicast-scouting \
-'By default the zenoh bridge listens and replies to UDP multicast scouting messages for being discovered by peers and routers.
-This option disables this feature.'"
-        ))
-        .arg(Arg::from_usage(
-r"--rest-http-port=[PORT | IP:PORT] \
-'Configures HTTP interface for the REST API (disabled by default, setting this option enables it). Accepted values:'
-  - a port number
-  - a string with format `<local_ip>:<port_number>` (to bind the HTTP server to a specific interface)."
-        ))
-        //
-        // DDS related arguments:
-        //
-        .arg(Arg::from_usage(
-r#"-n, --namespace=[String]   'A ROS 2 namespace to be used by the "zenoh_bridge_dds" node'"#
-        ))
-        .arg(Arg::from_usage(
-r#"-d, --domain=[ID]   'The DDS Domain ID. The default value is "$ROS_DOMAIN_ID" if defined, or "0" otherwise.'"#)
-            .default_value(&DEFAULT_DOMAIN_STR)
-        )
-        .arg(Arg::from_usage(
-r#"--ros-localhost-only \
-'Configure CycloneDDS to use only the localhost interface. If not set, CycloneDDS will pick the interface defined in "$CYCLONEDDS_URI" configuration, or automatically choose one.
-This option is not active by default, unless the "ROS_LOCALHOST_ONLY" environment variable is set to "1".'"#
-        ));
-
-    // Add option to enable DDS SHM if feature is enabled
-    #[cfg(feature = "dds_shm")]
-    {
-        app = app.arg(Arg::from_usage(
-                r#"--dds-enable-shm \
-                'Configure CycloneDDS to use Iceoryx shared memory. If not set, CycloneDDS will instead use any shared memory settings defined in "$CYCLONEDDS_URI" configuration.
-                This option is not active by default.'"#
-            ));
-    }
-
-    app = app
-        .arg(Arg::from_usage(
-r#"--pub-max-frequency=[String]...   'Specifies a maximum frequency of publications routing over zenoh for a set of Publishers.
-  The string must have the format "<regex>=<float>":
-  - "regex" is a regular expression matching a Publisher interface name
-  - "float" is the maximum frequency in Hertz; if publication rate is higher, downsampling will occur when routing.
-Repeat this option to configure several topics expressions with a max frequency.'"#
-        ))
-        .arg(Arg::from_usage(
-r#"--queries-timeout-default=[float]... 'A float in seconds (default: 5.0 sec) that will be used as a timeout when the bridge
-queries any other remote bridge for discovery information and for historical data for TRANSIENT_LOCAL DDS Readers it serves
-(i.e. if the query to the remote bridge exceed the timeout, some historical samples might be not routed to the Readers, but the route will not be blocked forever).
-This value overwrites the value possibly set in configuration file under 'plugins/ros2dds/queries_timeout/default' key."#
-        ))
-        .arg(Arg::from_usage(
-r#"--watchdog=[PERIOD]   'Experimental!! Run a watchdog thread that monitors the bridge's async executor and reports as error log any stalled status during the specified period (default: 1.0 second)'"#
-        ).default_missing_value("1.0"));
-    let args = app.get_matches();
-
-    // load config file at first
-    let mut config = match args.value_of("config") {
-        Some(conf_file) => Config::from_file(conf_file).unwrap(),
-        None => Config::default(),
-    };
-    // if "ros2dds" plugin conf is not present, add it (empty to use default config)
-    if config.plugin("ros2dds").is_none() {
-        config.insert_json5("plugins/ros2dds", "{}").unwrap();
-    }
-
-    // apply zenoh related arguments over config
-    // NOTE: only if args.occurrences_of()>0 to avoid overriding config with the default arg value
-    if args.occurrences_of("mode") > 0 {
-        config
-            .set_mode(Some(args.value_of("mode").unwrap().parse().unwrap()))
-            .unwrap();
-    }
-    if let Some(endpoints) = args.values_of("connect") {
-        config
-            .connect
-            .endpoints
-            .extend(endpoints.map(|p| p.parse().unwrap()))
-    }
-    if let Some(endpoints) = args.values_of("listen") {
-        config
-            .listen
-            .endpoints
-            .extend(endpoints.map(|p| p.parse().unwrap()))
-    }
-    if args.is_present("no-multicast-scouting") {
-        config.scouting.multicast.set_enabled(Some(false)).unwrap();
-    }
-    if let Some(port) = args.value_of("rest-http-port") {
-        config
-            .insert_json5("plugins/rest/http_port", &format!(r#""{port}""#))
-            .unwrap();
-    }
-    // Always add timestamps to publications (required for PublicationCache used in case of TRANSIENT_LOCAL topics)
-    config
-        .timestamping
-        .set_enabled(Some(ModeDependentValue::Unique(true)))
-        .unwrap();
-
-    // apply DDS related arguments over config
-    insert_json5!(config, args, "plugins/ros2dds/id", if "id",);
-    insert_json5!(config, args, "plugins/ros2dds/namespace", if "namespace",);
-    insert_json5!(config, args, "plugins/ros2dds/domain", if "domain", .parse::<u64>().unwrap());
-    insert_json5!(config, args, "plugins/ros2dds/ros_localhost_only", if "ros-localhost-only");
-    #[cfg(feature = "dds_shm")]
-    {
-        insert_json5!(config, args, "plugins/ros2dds/shm_enabled", if "dds-enable-shm");
-    }
-    insert_json5!(config, args, "plugins/ros2dds/pub_max_frequencies", for "pub-max-frequency", .collect::<Vec<_>>());
-    insert_json5!(config, args, "plugins/ros2dds/queries_timeout/default", if "queries-timeout-default", .parse::<f32>().unwrap());
-
-    let watchdog_period = if args.is_present("watchdog") {
-        args.value_of("watchdog").map(|s| s.parse::<f32>().unwrap())
-    } else {
-        None
-    };
-
-    (config, watchdog_period)
+fn parse_args() -> (Option<f32>, Config) {
+    let args = BridgeArgs::parse();
+    (args.watchdog.flatten(), args.into())
 }
 
 #[async_std::main]
@@ -200,7 +33,7 @@ async fn main() {
         *zenoh_plugin_ros2dds::LONG_VERSION
     );
 
-    let (config, watchdog_period) = parse_args();
+    let (watchdog_period, config) = parse_args();
     let rest_plugin = config.plugin("rest").is_some();
 
     if let Some(period) = watchdog_period {
