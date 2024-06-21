9e6cfba0d264fbe0e0d482c35e0715cddb4e4baf
Charles Schleich <schleich.cdaniel@gmail.com>
2024-06-21 15:22:11 +0200

    fixed build error when enabling dds_shm flag

--- zenoh-plugin-ros2dds/src/dds_types.rs
+++ zenoh-plugin-ros2dds/src/dds_types.rs
@@ -17,8 +17,6 @@ use std::fmt;
 use std::slice;
 use zenoh::bytes::ZBytes;
 use zenoh::encoding::Encoding;
-#[cfg(feature = "dds_shm")]
-use zenoh::internal::buffers::ZSlice;
 use zenoh::internal::Value;
 
 use crate::dds_utils::ddsrt_iov_len_to_usize;
@@ -215,10 +213,10 @@ impl From<&DDSRawSample> for ZBytes {
             // Where data was received via Iceoryx return both the header (contained in buf.data) and
             // payload (contained in buf.iox_chunk) in a buffer.
             if let Some(iox_chunk) = buf.iox_chunk {
-                let mut zbuf = ZBuf::default();
-                zbuf.push_zslice(ZSlice::from(buf.data_as_slice().to_vec()));
-                zbuf.push_zslice(ZSlice::from(iox_chunk.as_slice().to_vec()));
-                return zbuf;
+                let mut buf_and_iox_chunk = buf.data_as_slice().to_vec();
+                buf_and_iox_chunk.append(&mut iox_chunk.as_slice().to_vec());
+                let z_bytes = ZBytes::from(buf_and_iox_chunk);
+                return z_bytes;
             }
         }
         buf.data_as_slice().to_vec().into()
