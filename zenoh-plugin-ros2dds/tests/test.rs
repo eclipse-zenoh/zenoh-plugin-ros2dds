5bb9dd7c0fa1b3eaff2e4af1a74c590a7ad23b04
ChenYing Kuo <evshary@gmail.com>
2024-12-04 17:40:42 +0800

    Add Zenoh publisher and ROS subscriber.
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

--- zenoh-plugin-ros2dds/tests/test.rs
+++ zenoh-plugin-ros2dds/tests/test.rs
@@ -14,14 +14,20 @@
 
 #[cfg(feature = "ros_test")]
 mod ros_test {
-    use std::time::Duration;
+    use std::{sync::mpsc::channel, time::Duration};
 
+    use futures::StreamExt;
     use r2r::{self, QosProfile};
     use zenoh::{
         config::Config,
         internal::{plugins::PluginsManager, runtime::RuntimeBuilder},
     };
     use zenoh_config::ModeDependentValue;
+
+    // The test topic
+    const TEST_TOPIC: &str = "test_topic";
+    // The test TEST_PAYLOAD
+    const TEST_PAYLOAD: &str = "Hello World";
 
     async fn create_bridge() {
         let mut plugins_mgr = PluginsManager::static_plugins_only();
@@ -44,25 +50,66 @@ mod ros_test {
     }
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn test_ros_pub_zenoh_sub() {
-        let topic_name = "mytopic";
-        let payload = "hello world";
+    async fn test_zenoh_pub_ros_sub() {
+        let (tx, rx) = channel();
 
+        // Create zenoh-bridge-ros2dds
+        tokio::spawn(create_bridge());
+
+        // ROS subscriber
+        let ctx = r2r::Context::create().unwrap();
+        let mut node = r2r::Node::create(ctx, "ros_sub", "").unwrap();
+        let subscriber = node
+            .subscribe::<r2r::std_msgs::msg::String>(
+                &format!("/{}", TEST_TOPIC),
+                QosProfile::default(),
+            )
+            .unwrap();
+
+        // Zenoh publisher
+        let session = zenoh::open(zenoh::Config::default()).await.unwrap();
+        let publisher = session.declare_publisher(TEST_TOPIC).await.unwrap();
+
+        // Wait for the environment to be ready
+        tokio::time::sleep(Duration::from_secs(1)).await;
+
+        // Publish Zenoh message
+        let buf = cdr::serialize::<_, _, cdr::CdrLe>(TEST_PAYLOAD, cdr::size::Infinite).unwrap();
+        publisher.put(buf).await.unwrap();
+
+        // Check ROS subscriber will receive the data
+        tokio::spawn(async move {
+            subscriber
+                .for_each(|msg| {
+                    tx.send(msg.data).unwrap();
+                    futures::future::ready(())
+                })
+                .await
+        });
+        node.spin_once(std::time::Duration::from_millis(100));
+        let data = rx
+            .recv_timeout(Duration::from_secs(3))
+            .expect("Receiver timeout");
+        assert_eq!(data, TEST_PAYLOAD);
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_ros_pub_zenoh_sub() {
         // Create zenoh-bridge-ros2dds
         tokio::spawn(create_bridge());
 
         // Zenoh subscriber
         let session = zenoh::open(zenoh::Config::default()).await.unwrap();
-        let subscriber = session.declare_subscriber(topic_name).await.unwrap();
+        let subscriber = session.declare_subscriber(TEST_TOPIC).await.unwrap();
 
         // ROS publisher
         let ctx = r2r::Context::create().unwrap();
-        let mut node = r2r::Node::create(ctx, "testnode", "").unwrap();
+        let mut node = r2r::Node::create(ctx, "ros_pub", "").unwrap();
         let publisher = node
-            .create_publisher(&format!("/{}", topic_name), QosProfile::default())
+            .create_publisher(&format!("/{}", TEST_TOPIC), QosProfile::default())
             .unwrap();
         let msg = r2r::std_msgs::msg::String {
-            data: payload.into(),
+            data: TEST_PAYLOAD.into(),
         };
 
         // Wait for the environment to be ready
@@ -77,7 +124,7 @@ mod ros_test {
             let result: Result<String, _> =
                 cdr::deserialize_from(sample.payload().reader(), cdr::size::Infinite);
             let recv_data = result.expect("Fail to receive data");
-            assert_eq!(recv_data, payload);
+            assert_eq!(recv_data, TEST_PAYLOAD);
         })
         .await
         .expect("Timeout: Zenoh subscriber didn't receive any ROS message.");
